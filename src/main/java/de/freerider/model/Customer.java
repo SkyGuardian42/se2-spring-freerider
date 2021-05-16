4a171d03d442db26fe0d64805249cd2e3334d1a0
Malte <skyguardian42@gmail.com>
2021-05-16 17:51:00 +0200

    added repository and model

---
+++ src/main/java/de/freerider/model/Customer.java
@@ -0,0 +1,65 @@
+package de.freerider.model;
+
+public class Customer {
+	String id;
+	String lastName;
+	String firstName;
+	String contact;
+	Status status;
+
+	public Customer(String lastName, String firstName, String contact) {
+		this.lastName = lastName;
+		this.firstName = firstName;
+		this.contact = contact;
+		this.id = null;
+		this.status = Status.New;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getContact() {
+		return contact;
+	}
+
+	public void setContact(String contact) {
+		this.contact = contact;
+	}
+
+	public Status getStatus() {
+		return status;
+	}
+
+	public void setStatus(Status status) {
+		this.status = status;
+	}
+
+	enum Status {
+		New,
+		InRegistration,
+		Active,
+		Suspended,
+		Deleted
+	}
+}
